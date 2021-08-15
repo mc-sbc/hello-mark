d26b1cbce8977866438308228e8d99ee4ee12966
mc-sbc <88698437+mc-sbc@users.noreply.github.com>
2021-08-15 16:54:44 +1000

    Changes made to hello.py. Added personalised message.

--- hello.py
+++ hello.py
@@ -1,2 +1,3 @@
 if __name__ == '__main__':
-    print('Hello everyone :)')
+    print("Hello fellow programmers, it's Mark here!")
+    print('Just enjoying a lazy Sunday afternoon in lockdown 6.0!')
