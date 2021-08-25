b163b651cca0a59fa45c7baa9f5da017c6a74c3b
mc-sbc <88698437+mc-sbc@users.noreply.github.com>
2021-08-25 13:05:23 +1000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
 if __name__ == '__main__':
     print("Hello fellow programmers, it's Mark here!")
-    print('Just enjoying a lazy Sunday afternoon in lockdown 6.0!')
+    print('Just enjoying a lazy Sunday afternoon in lockdown 6.0!!!')
